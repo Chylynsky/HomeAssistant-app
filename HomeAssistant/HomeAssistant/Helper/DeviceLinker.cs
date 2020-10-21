41501520ba68e4f739cc983110129d3ce1be09a7
Borys Chylinski <borchy97@gmail.com>
2020-10-22 00:16:10 +0200

    Add doc-comments for the DeviceLinker.

--- HomeAssistant/HomeAssistant/Helper/DeviceLinker.cs
+++ HomeAssistant/HomeAssistant/Helper/DeviceLinker.cs
@@ -11,10 +11,16 @@ namespace HomeAssistant.Helper
 {
     static internal class DeviceLinker
     {
+        /// <summary>
+        /// Get object of a Model class being the implementation of IDeviceModel interface. Target type is
+        /// deduced based on the deviceTypeName parameter.
+        /// </summary>
+        /// <param name="deviceTypeName">Name of the type.</param>
+        /// <returns>Object of the Model corresponding to the given name.</returns>
         public static IDeviceModel GetDeviceModelForTypeName(string deviceTypeName)
         {
             string assemblyName = Assembly.GetCallingAssembly().FullName;
-            string fullTypeName = string.Format("HomeAssistant.Model.{0}Model", deviceTypeName);
+            string fullTypeName = string.Format("HomeAssistant.Model.Devices.{0}Model", deviceTypeName);
             Type deviceModelType = Type.GetType(string.Format(CultureInfo.InvariantCulture, "{0}, {1}", fullTypeName, assemblyName));
 
             if (deviceModelType == null)
@@ -25,10 +31,15 @@ namespace HomeAssistant.Helper
             return Activator.CreateInstance(deviceModelType) as IDeviceModel;
         }
 
-        public static IDeviceViewModel GetDeviceViewModelForModel(Type deviceModel)
+        /// <summary>
+        /// Get ViewModel object corresponding to the given Model.
+        /// </summary>
+        /// <param name="deviceModel">Model for which the ViewModel is to be created.</param>
+        /// <returns>ViewModel object being an implementation of the IDeviceViewModel interface.</returns>
+        public static IDeviceViewModel GetDeviceViewModelForModel(IDeviceModel deviceModel)
         {
-            string assemblyName = deviceModel.GetTypeInfo().Assembly.FullName;
-            string viewModelName = deviceModel.FullName.Replace("Model", "ViewModel");
+            string assemblyName = deviceModel.GetType().GetTypeInfo().Assembly.FullName;
+            string viewModelName = deviceModel.GetType().FullName.Replace("Model", "ViewModel");
 
             Type deviceViewModelType = Type.GetType(string.Format(CultureInfo.InvariantCulture, "{0}, {1}", viewModelName, assemblyName));
 
@@ -37,9 +48,14 @@ namespace HomeAssistant.Helper
                 throw new Exception(string.Format("Type {0} does not exist in assembly {1}.", viewModelName, assemblyName));
             }
 
-            return Activator.CreateInstance(deviceViewModelType) as IDeviceViewModel;
+            return Activator.CreateInstance(deviceViewModelType, deviceModel) as IDeviceViewModel;
         }
 
+        /// <summary>
+        /// Get View corresponding to the given ViewModel.
+        /// </summary>
+        /// <param name="deviceViewModel">Type of the ViewModel.</param>
+        /// <returns>ContentView object being the View for the specified ViewModel.</returns>
         public static ContentView GetDeviceViewForViewModel(Type deviceViewModel)
         {
             string assemblyName = deviceViewModel.GetTypeInfo().Assembly.FullName;
